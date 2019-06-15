78040ca45f55af7c9891bd091adc66a5e104fc17
张博 <zhangbo819@163.com>
2019-06-15 11:32:51 +0800

    fixed add babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: { 
+    entry: {
         app: ['./index.js']
-     },
-    devServer: {
-        contentBase: './dist',
-        hot: true
     },
     plugins: [
         // new CleanWebpackPlugin(),
@@ -20,6 +16,9 @@ module.exports = {
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
     ],
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
@@ -27,9 +26,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"
-                // test 符合此正则规则的文件，运用 loader 去进行处理，除了exclude 中指定的内容
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
             }
         ]
-    }
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
 };
